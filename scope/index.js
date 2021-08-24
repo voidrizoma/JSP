aca9c93636f6c91141bfec18565f9ba27184a596
voidrizoma <milhadasdepapel@gmail.com>
2021-08-23 21:06:01 -0500

    scope

---
+++ scope/index.js
@@ -0,0 +1,29 @@
+//Global Scope
+//Window es un scope de àmbito global
+let message = "hello";
+
+function printNumber() {
+  let i;
+  for (i = 0; i < 10; i++) {
+    function eventualyPrint(n) {
+      setTimeout(() => {
+        console.log(n);
+      }, 1000);
+    }
+    eventualyPrint(i);
+  }
+}
+
+printNumber();
+
+//Blok scope
+
+function printNumbers2() {
+    for (let i = 0; i < 10; i++) {
+      setTimeout(function() {
+        console.log(i);
+      }, 100);
+    }
+  }
+
+  printNumbers2();
